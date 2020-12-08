41ce7817384060ee0d5e66b11749110f9838766e
songzeshan <mountaincnc@gmail.com>
2020-12-08 09:45:46 +0800

    Add week03 work

---
+++ Week03/cmd/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"golang.org/x/sync/errgroup"
+	"net/http"
+	"os"
+	"os/signal"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request) {
+		fmt.Fprintln(resp, "Hello World")
+	})
+
+	g, ctx := errgroup.WithContext(context.Background())
+	// 启动http server
+	g.Go(func() error {
+		return startServer(ctx, ":8080", mux)
+	})
+
+	// 监听signal
+	g.Go(func() error {
+		return sysSig(ctx)
+	})
+
+	// 等待错误
+	if err := g.Wait(); err != nil {
+		fmt.Printf("service exited %s\n", err.Error())
+	}
+
+}
+
+func startServer(ctx context.Context, addr string, handler http.Handler) error {
+	s := &http.Server{
+		Addr:              addr,
+		Handler:           handler,
+	}
+
+	// 监听context
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				fmt.Printf("recovered: %v\n", r)
+			}
+		}()
+		<- ctx.Done()
+		fmt.Println("get system signal, shutdown http server")
+
+		// 优雅关闭http server
+		s.Shutdown(context.Background())
+	}()
+
+	return s.ListenAndServe()
+}
+
+func sysSig(ctx context.Context) error {
+	// 创建信号chan
+	sigCh := make(chan os.Signal)
+
+	// 监听signal
+	signal.Notify(sigCh)
+
+	for {
+		select {
+		case <- ctx.Done():
+			return fmt.Errorf("http server exited")
+		case s := <-sigCh:
+			return fmt.Errorf("get signal: %v\n", s)
+		}
+	}
+}
